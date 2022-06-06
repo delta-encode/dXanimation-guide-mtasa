06396f7aaf59d567b50abbe48c8ccfdeb8a28fde
Berke Saygın <97914973+deltathewoo@users.noreply.github.com>
2022-06-06 23:42:45 +0300

    Update client.lua

--- guide/client.lua
+++ guide/client.lua
@@ -1,24 +1,24 @@
-local menuVisible = false --- panel kapalı
-local scx, scy = guiGetScreenSize()
-local px = scx/1920
-local sizeX, sizeY = 400*px, 500*px
-local posX, posY = scx/2-sizeX/2, scy/2-sizeY/2
+local menuVisible = false
+local x, y = guiGetScreenSize()
+local px = x/1920
+local sx, sy = 400*px, 500*px
+local psx, psy = x/2-sx/2, y/2-sy/2
 local enableAlpha = false
 local alpha = 0
 
 function menuDraw()
-	if enableAlpha == true then  --- eğer "alpha" değişkeni aktif edilmişse / if açıldı.
-		if alpha <= 250 then --- eğer şeffaflık(alpha) 250 sayısından az ise
-			alpha = alpha + 5 --- 250 sayısına ulaşana kadar, 5 ekle
+	if enableAlpha == true then
+		if alpha <= 250 then
+			alpha = alpha + 5
 		end
-	elseif enableAlpha == false then --- eğer "alpha" değişkeni aktif değilse
-		if alpha >= 5 then --- eğer "alpha" değişkeni 5 ten büyük ise
-			alpha = alpha - 5 --- "alpha" değişkeni 5 olana kadar 5 eksilt
+	elseif enableAlpha == false then
+		if alpha >= 5 then
+			alpha = alpha - 5
 		end
-	end -- if kapandı
-	dxDrawRectangle(posX, posY, sizeX, sizeY, tocolor(0,0,0,alpha))
-	if alpha <= 0 then --- eğer "alpha" değişkeni 0 ise
-		removeEventHandler("onClientRender",root,menuDraw) --- Oluşturulan menüyü kapat
+	end
+	dxDrawRectangle(psx, psy, sx, sy, tocolor(0,0,0,alpha))
+	if alpha <= 0 then
+		removeEventHandler("onClientRender",root,menuDraw)
 	end
 end
 
